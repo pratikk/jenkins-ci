c308c51424707ff6a8db0b60e95dec303fc2477e
Pratik Konnur <konnup2@TDD00659134.TDBFG.com>
2014-08-18 13:42:29 -0400

    Integrated springtest maven files into this project

---
+++ jenkins/src/main/java/com/jenkins/app/Author.java
@@ -0,0 +1,20 @@
+package com.jenkins.app;
+
+public class Author {
+	private String name;
+	private int pubs;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPubs() {
+		return pubs;
+	}
+	public void setPubs(int pubs) {
+		this.pubs = pubs;
+	}
+
+}
---
+++ jenkins/src/main/java/com/jenkins/app/Story.java
@@ -0,0 +1,36 @@
+package com.jenkins.app;
+
+public class Story {
+	 
+	private String title;
+	private Author author;
+	private String content;
+	private String pubDate;
+ 
+	public Story () {
+	}	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+	public Author getAuthor() {
+		return author;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getContent() {
+		return content;
+	}
+	public String getPubDate() {
+		return pubDate;
+	}
+	public void setPubDate(String pubDate) {
+		this.pubDate = pubDate;
+	}
+}
